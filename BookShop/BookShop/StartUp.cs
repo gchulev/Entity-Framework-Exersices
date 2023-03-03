54ef082dbcdabb0972b78636f2e142f0cd246459
gchulev <georgi.chulev@gmail.com>
2023-03-03 07:42:22 +0200

    Added Exercise: '02. Age Restriction'

---
+++ BookShop/BookShop/StartUp.cs
@@ -0,0 +1,38 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+
+    using Data;
+    using Initializer;
+
+    using Microsoft.EntityFrameworkCore;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            Console.Write("Enter age restriction: ");
+            string cmd = Console.ReadLine()!;
+
+            Console.WriteLine(GetBooksByAgeRestriction(db, cmd));
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var bookTitles = context.Books
+                .AsNoTracking()
+                .ToArray()
+                .Where(b => b.AgeRestriction.ToString().ToLower() == command.ToLower())
+                .Select(b => b.Title)
+                .OrderBy(b => b)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, bookTitles).Trim();
+        }
+    }
+}
+
+
