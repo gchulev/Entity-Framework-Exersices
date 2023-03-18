fc519b3c52622ccaa00e5c0766ef70d247912bff
gchulev <georgi.chulev@gmail.com>
2023-03-18 19:49:49 +0200

    Added exercise: XML - '01. Import Users'

---
+++ ProductShopXml/ProductShop/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System.Xml;
+using System.Xml.Serialization;
+
+using AutoMapper;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using ProductShop.Data;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+using ProductShop.Utilities;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var context = new ProductShopContext())
+            {
+                string inputXmlPath = @"D:\Visual Studio\Projects\EntityFrameWork Exersices\ProductShopXml\ProductShop\Datasets\users.xml";
+
+                Console.WriteLine(ImportUsers(context, inputXmlPath));
+            }
+
+        }
+        private static IMapper CreateMapper()
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<ProductShopProfile>());
+
+            return config.CreateMapper();
+        }
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            IMapper mapper = CreateMapper();
+
+            using (StreamReader stream = new StreamReader(inputXml))
+            {
+                
+                ImportUserDto[] users = XmlHelper.Deserialize<ImportUserDto[]>(inputXml, "Users");
+
+                User[] importUsers = users.Select(u => mapper.Map<User>(u)).ToArray();
+
+                context.Users.AddRange(importUsers);
+                context.SaveChanges();
+
+                return $"Successfully imported {users.Length}";
+            }
+
+        }
+    }
+}
---
+++ ProductShopXml/ProductShop/Utilities/XmlHelper.cs
@@ -0,0 +1,22 @@
+﻿using ProductShop.DTOs.Import;
+using System.Xml.Serialization;
+
+namespace ProductShop.Utilities
+{
+    internal class XmlHelper
+    {
+        public static T Deserialize<T>(string inputXml, string rootElm)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute(rootElm);
+
+            XmlSerializer serializer = new XmlSerializer(typeof(T), xmlRoot);
+
+            using (StreamReader reader = new StreamReader(inputXml))
+            {
+                T serializedObj = (T)serializer.Deserialize(reader)!;
+
+                return serializedObj;
+            }
+        }
+    }
+}
