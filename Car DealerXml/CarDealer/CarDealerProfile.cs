9447f94eb6650861999dc1ddadfa85b4bfc674f9
gchulev <georgi.chulev@gmail.com>
2023-03-20 09:12:00 +0200

    Added Exsercise: XML - '09. Import Suppliers'

--- Car DealerXml/CarDealer/CarDealerProfile.cs
+++ Car DealerXml/CarDealer/CarDealerProfile.cs
@@ -1,12 +1,16 @@
 ﻿using AutoMapper;
 
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+
 namespace CarDealer
 {
     public class CarDealerProfile : Profile
     {
         public CarDealerProfile()
         {
-
+            // Supplier
+            CreateMap<ImportSupplierDto, Supplier>();
         }
     }
 }
--- Car DealerXml/CarDealer/StartUp.cs
+++ Car DealerXml/CarDealer/StartUp.cs
@@ -1,8 +1,12 @@
 ﻿using AutoMapper;
 
 using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
 
 using Microsoft.Extensions.DependencyInjection;
+
+using ProductShop.Utilities;
 
 namespace CarDealer
 {
@@ -12,15 +16,35 @@ namespace CarDealer
         {
             using (var context = new CarDealerContext())
             {
-                var serviceCollection = new ServiceCollection();
-                serviceCollection.AddTransient<IMapper, Mapper>();
-                var provider = serviceCollection.BuildServiceProvider();
+                string filePath = @"D:\Visual Studio\Projects\EntityFrameWork Exersices\Car DealerXml\CarDealer\Datasets\suppliers.xml";
+                string inputXml = File.ReadAllText(filePath);
 
-
-
+                Console.WriteLine(ImportSuppliers(context, inputXml));
             }
 
         }
-        
+
+        private static IMapper CreateMapper()
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<CarDealerProfile>());
+
+            IMapper mapper = config.CreateMapper();
+
+            return mapper;
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            IMapper mapper = CreateMapper();
+
+            ImportSupplierDto[] suppliersDto = XmlHelper.Deserialize<ImportSupplierDto[]>(inputXml, "Suppliers");
+
+            Supplier[] suppliers = mapper.ProjectTo<Supplier>(suppliersDto.AsQueryable()).ToArray();
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Length}";
+        }
     }
 }
